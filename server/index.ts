e4fea8ed6fd31713ef5462d1490f159773db4112
wally <j.sebastian.aw@gmail.com>
2025-06-11 17:59:50 -0500

    Remove server/src directory files including .gitignore, index.ts, package.json, package-lock.json, and tsconfig.json, resulting in a clean slate for the project structure.

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,18 @@
 import express from 'express';
+import { env } from 'node:process';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port= env.PORT;
 
 app.get('/get', (req, res) => {
   res.json({ message: 'Hello World' });
   console.log(res.statusCode);
-  
 });
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Server' });
-    console.log(res.statusCode)
-  });
+  res.json({ message: 'Server' });
+  console.log(res.statusCode);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
